b5c19646b2e46d7fd5614aa8c4075c9dc6188a99
GuillaumeSco <47627578+GuillaumeSco@users.noreply.github.com>
2020-04-16 14:35:09 +0200

    Ajout du css pour la page Admin

--- connexion_Admin.php
+++ connexion_Admin.php
@@ -33,172 +33,219 @@ if (isset($_POST['button1'])) {
 <html>
 <head>
 	<title>ECE Ebay</title>
+	<a href="index.html">retour</a>
 	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="connexion_Admin.css">
+	<div class="app-title">
+		<h1>Connecté en tant qu'admin</h1>
+	</div>
 </head>
 <body>
-	<?php
-	if ($db_found) {
-		$sql = "SELECT ID_vendeur FROM vendeur";
-		$result = mysqli_query($db_handle, $sql);
-		if (mysqli_num_rows($result) == 0) {
-			echo "pas de vendeur";
+	
+
+	<div class="login">
+		<div class="login-screen">
+			<div class="app-title">
+				<h1>Suppression</h1>
+			</div>
+			<div class="login-form">
+				<?php
+				if ($db_found) {
+					$sql = "SELECT ID_vendeur FROM vendeur";
+					$result = mysqli_query($db_handle, $sql);
+					if (mysqli_num_rows($result) == 0) {
+						echo "pas de vendeur";
 			//exit();
-		} else {
-			while ($data = mysqli_fetch_assoc($result)) {	
-				echo "<form action=supprimer_vendeur_admin.php method=post>";
-				echo "<table>";
-				echo "<tr>";
-				echo " <td><input type=radio name=suppr value=". $data['ID_vendeur'] . ">";
+					} 
+					else {
+						while ($data = mysqli_fetch_assoc($result)) {	
+							echo "<form action=supprimer_vendeur_admin.php method=post>";
+							echo "<table>";
+							echo "<tr>";
+							echo " <td><input type=radio name=suppr value=". $data['ID_vendeur'] . ">";
 				//	"<label for=". $data['ID_item'] . "> ". $data['ID_item'] . "</label>";
-				echo "ID vendeur : " . $data['ID_vendeur'] . "<br>";
-				echo "</tr>";
+							echo "ID vendeur : " . $data['ID_vendeur'] . "<br>";
+							echo "</tr>";
 					//echo "Photo de profil :". "<img src=".$data['Photo']." />" . "<br>";
 
-			}
-			echo "<tr></tr>";
-			echo "<td colspan=2 align=center>";
-			
-			echo "<input type=submit name=button3 value=supprimer></td></tr>
-			
-			</table>";
-			echo "</form>";
-		}
-	}
+						}
+						echo "<tr></tr>";
+						echo "</table>";
+
+						echo "<input class=btn btn-primary btn-large btn-block type=submit name=button3 value=supprimer>";
+						echo "</form>";
+					}
+				}
 
 
-if ($db_found) {
-		$sql = "SELECT ID_item FROM Item";
-		$result = mysqli_query($db_handle, $sql);
-		if (mysqli_num_rows($result) == 0) {
-			echo "pas d'item";
+				if ($db_found) {
+					$sql = "SELECT ID_item FROM Item";
+					$result = mysqli_query($db_handle, $sql);
+					if (mysqli_num_rows($result) == 0) {
+						echo "pas d'item";
 			//exit();
-		} else {
-			while ($data = mysqli_fetch_assoc($result)) {	
-				echo "<form action=supprimer_item_admin.php method=post>";
-				echo "<table>";
-				echo "<tr>";
-				echo " <td><input type=radio name=suppr value=". $data['ID_item'] . ">";
+					} else {
+						while ($data = mysqli_fetch_assoc($result)) {	
+							echo "<form action=supprimer_item_admin.php method=post>";
+							echo "<table>";
+							echo "<tr>";
+							echo " <td><input type=radio name=suppr value=". $data['ID_item'] . ">";
 				//	"<label for=". $data['ID_item'] . "> ". $data['ID_item'] . "</label>";
-				echo "ID item : " . $data['ID_item'] . "<br>";
-				echo "</tr>";
+							echo "ID item : " . $data['ID_item'] . "<br>";
+							echo "</tr>";
 					//echo "Photo de profil :". "<img src=".$data['Photo']." />" . "<br>";
 
-			}
-			echo "<tr></tr>";
-			echo "<td colspan=2 align=center>";
-			
-			echo "<input type=submit name=button3 value=supprimer></td></tr>
-			
-			</table>";
-			echo "</form>";
+						}
+						echo "<tr></tr>";
+						echo "</table>";
+
+						echo "<input class=btn btn-primary btn-large btn-block type=submit name=button3 value=supprimer>";
+						echo "</form>";
 
 
 
 
 
-		}
-	}
+					}
+				}
+				?>
+			</div>
+		</div>
+	</div>
+
+	<div class="login">
+		<div class="login-screen">
+			<div class="app-title">
+				<table>
+					<h1>Ajouter un vendeur</h1>
+				</div>
+
+				<div class="login-form">
+					<div class="control-group">
+
+						<form action="ajouter_vendeur.php" method="post">
+
+							<tr align="center">
+								<td>Pseudo :</td>
+								<td align="center"><input type="text" name="pseudo"></td>
+							</tr>
+							<tr align="center">
+								<td>Email :</td>
+								<td align="center"><input type="email" name="email"></td>
+							</tr >
+							<tr align="center">
+								<td>Nom :</td>
+								<td><input type="text" name="nom"></td>
+							</tr>
+							<tr align="center">
+								<td>Image de fond (URL) :</td>
+								<td><input type="text" name="image"></td>
+							</tr>
+							<tr align="center">
+								<td>Photo de profil (URL) :</td>
+								<td><input type="text" name="photo"></td>
+							</tr>
+							<tr></tr>
+						</table><br>
+
+						<input class="btn btn-primary btn-large btn-block" type="submit" name="button1" value="ajouter">
 
 
 
-	?>
-	<h2>Connecté en tant qu'admin</h2>
-	<form action="ajouter_vendeur.php" method="post">
-		<table>
-			<tr>
-				<td>Pseudo :</td>
-				<td><input type="text" name="pseudo"></td>
-			</tr>
-			<tr>
-				<td>Email :</td>
-				<td><input type="email" name="email"></td>
-			</tr>
-			<tr>
-				<td>Nom :</td>
-				<td><input type="text" name="nom"></td>
-			</tr>
-			<tr>
-				<td>Image :</td>
-				<td><input type="text" name="image"></td>
-			</tr>
-			<tr>
-				<td>Photo :</td>
-				<td><input type="text" name="photo"></td>
-			</tr>
-			<tr></tr>
-			<tr>
-				<td colspan="2" align="center">
-					<input type="submit" name="button1" value="ajouter">	
-				</td>
-			</tr>
-		</table>
+
+
+
+					</div>
+				</div>
+			</div>
+		</div>
 	</form>
 </body>
 </html>
 
-<h1>Ajouter un item</h1>
-	<form action="ajouter_item_admin.php" method="post">
-		<table>
-			<tr>
-				<td><input type="radio" id="ferraille ou tresor" name="categorie" value="ferraille ou tresor" checked>
-					<label for="ferraille ou tresor"> ferraille ou tresor</label>
-				</tr>
-				<tr>
-					<td><input type="radio" id="bon pour le musee" name="categorie" value="bon pour le musee" checked>
-						<label for="bon pour le musee"> bon pour le musee</label>
-					</tr>
-					<tr>
-						<td><input type="radio" id="accessoire VIP" name="categorie" value="accessoire VIP" checked>
-							<label for="accessoire VIP"> accessoire VIP</label>
-						</tr>
+
+<form action="ajouter_item_admin.php" method="post">
+	<div class="login">
+		<div class="login-screen">
+			<div class="app-title">
+				<table>
+					<h1>Ajouter un item</h1>
+				</div>
+
+				<div class="login-form">
+
+					<div class="control-group">
+
 						<tr>
-							<td>Nom :</td>
-							<td><input type="text" name="nom"></td>
-						</tr>
-						<tr>
-							<td>Photo :</td>
-							<td><input type="text" name="photo"></td>
-						</tr>
-						<tr>
-							<td>Description :</td>
-							<td><input type="text" name="description"></td>
-						</tr>
-						<tr>
-							<td>Vidéo :</td>
-							<td><input type="text" name="video"></td>
-						</tr>
-						<tr>
-							<td>Prix :</td>
-							<td><input type="number" step="0.01" name="prix"></td>
+							<td><label for="ferraille ou tresor"> Ferraille ou trésor</label></td>
+
+							<td><input type="radio" id="ferraille ou tresor" name="categorie" value="ferraille ou tresor" checked></td>
+
 						</tr>
 
 						<tr>
-							<td><input type="radio" id="vente par meilleure offre" name="vente" value="vente par meilleure offre" checked>
-								<label for="vente par meilleure offre"> vente par meilleure offre</label>
-							</tr>
-							<tr>
-								<td><input type="radio" id="vente par enchere" name="vente"  value="vente par enchere" checked>
-									<label for="vente par enchere"> vente par enchere</label>				
-								</tr>
-								<tr>
-									<td><input type="radio" id="aucun système de vente" name="vente"  value="aucun système de vente" checked>
-										<label for="aucun système de vente"> aucun système de vente</label>
-									</tr>
-									<tr>
-										<td><input type="checkbox" id="1" name="achat_imm" value="1" checked>
-											<label for="achat immediat"> achat immédiat</label>
-										</tr>
+							<td><label for="bon pour le musee"> Bon pour le musee</label></td>
+							<td><input type="radio" id="bon pour le musee" name="categorie" value="bon pour le musee" checked></td>
+
+						</tr>
+
+						<tr>	
+							<td>						<label for="accessoire VIP"> Accessoire VIP</label></td>
+							<td>						<input type="radio" id="accessoire VIP" name="categorie" value="accessoire VIP" checked></td>
+						</tr>
+
+						<tr>
+							<td>						Nom :</td>
+							<td>						<input type="text" name="nom"></td>
+						</tr>
+						<tr>
+							<td>						Photo :</td>
+							<td>						<input type="text" name="photo"></td>
+						</tr>
+						<tr>
+							<td>						Description :</td>
+							<td>						<input type="textarea" name="description"></td>
+						</tr>
+						<tr>
+							<td>						Vidéo :</td>
+							<td>						<input type="text" name="video"></td>
+						</tr>
+						<tr>
+							<td>						Prix :</td>
+							<td>						<input type="number" step="0.01" name="prix"></td>
+						</tr>
+						<tr>
+							<td>						<label for="vente par meilleure offre"> Vente par meilleure offre</label></td>
+							<td>						<input type="radio" id="vente par meilleure offre" name="vente" value="vente par meilleure offre" checked></td>
+						</tr>					
+						<tr>
+							<td>						<label for="vente par enchere"> Vente par enchère</label></td>	
+							<td>						<input type="radio" id="vente par enchere" name="vente"  value="vente par enchere" checked></td>
+						</tr>
+						<tr>
+							<td>						<label for="aucun système de vente"> Aucun système de vente</label></td>
+							<td>						<input type="radio" id="aucun système de vente" name="vente"  value="aucun système de vente" checked></td>
+						</tr>
+						<tr>
+							<td>						<label for="achat immediat"> Achat immédiat</label></td>
+							<td>						<input type="checkbox" id="1" name="achat_imm" value="1" checked></td>
+						</tr>						
 
 
-										<tr></tr>
-										<tr>
-											<td colspan="2" align="center">
-												<input type="submit" name="button2" value="ajouter">	
-											</td>
-										</tr>
-									</table>
-								</form>
+
+					</div>	
+				</table><br>
+
+				<input class="btn btn-primary btn-large btn-block" type="submit" name="button2" value="ajouter">
+
+			</div>
+		</div>
+	</div>
+</div>
 
 
-							</body>
-							</html>
+</form>
+
+
+</body>
+</html>
